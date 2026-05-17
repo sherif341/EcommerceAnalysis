8b272597430ddcfa9e838f8520aef1b356a93b01
Sherif Arafa <sherifarfa25@gmail.com>
2026-05-17 21:15:43 +0300

    deleted main file

--- main.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-df= pd.DataFrame({'Name':['Sherif','Omar','Karim','Gamal','Ahmed'],
-                  'Job Title':['Data Analyst','Dentist','Engineer','Businessman','Accountant']})
-filter =df[df['Job Title'].str.contains('Engineer')]
-print(filter)
