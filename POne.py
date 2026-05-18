7317b20e3ac732834c0e5f652521f1fd646b601f
Sherif Arafa <sherifarfa25@gmail.com>
2026-05-18 16:05:58 +0300

    make columns type

--- POne.py
+++ POne.py
@@ -4,7 +4,7 @@ import numpy as np
 from datetime import datetime
 
 from pandas import set_option
-
+#Reading The data
 df=pd.read_excel('ecommerce_orders.xlsx')
 set_option('display.max_rows', 500)
 set_option('display.max_columns', 500)
@@ -12,13 +12,16 @@ set_option('display.width', 1000)
 # print(df.shape[1])
 # print(df.info())
 # print(df.describe(include='all').T)
+
+#Data preprocessing
 df.columns=df.columns.str.strip().str.lower()
-# print(df['product'])
 text_columns=["customer_name","order_id","product","category","payment_method","status"]
 
 for column in text_columns:
     df[column]=df[column].astype(str).str.strip()
     df[column]=df[column].replace('nan',np.nan)
     df[column]=df[column].str.title()
-print(df)
 
+df['order_date']=pd.to_datetime(df['order_date'],errors='coerce')
+df['quantity']=pd.to_numeric(df['quantity'],errors='coerce').astype("Int64")
+print(df['quantity'])
